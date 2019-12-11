f3cb62d7cc22065d3a2d38f058e1aba2a5615ae3
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-11 14:57:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = {name: "Grace Hooper"}
+  my_hash_creator = {:name => "Grace Hooper"}
   # return a hash that includes the key and value parameters passed into this method
 end
 
