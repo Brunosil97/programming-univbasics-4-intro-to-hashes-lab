76c27eaab6822b1b96e843ae338d7a06a009761a
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-11 14:39:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def my_hash_creator(key, value)
   my_hash_creator[key, value] 
   # return a hash that includes the key and value parameters passed into this method
 end
-
+my_hash_creator[key, value] 
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
